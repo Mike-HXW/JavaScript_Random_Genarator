9ec6013b6ca7ada55be8bbe941c58851687c7bcc
Mike <hxwang813@gmail.com>
2024-11-24 20:11:41 -0500

    Library feature added

--- script.js
+++ script.js
@@ -2,35 +2,40 @@ let name = document.getElementById("nameInput");
 let button = document.getElementById("button");
 let output = document.getElementById("output");
 let fortunes = [
-        'You will have a nice day!',
-        'You will be happy an entire day!',
-        'You will meet someone you miss a lot!',
-        'You will be lucky for all days long!',
-        'You will overcome any difficulties you meet!',
-        'You will meet the uncertainty that can change your days!',
-        'You will get your efforts paid back pretty soon!'
-    ];
+    'You will have a nice day!',
+    'You will be happy an entire day!',
+    'You will meet someone you miss a lot!',
+    'You will be lucky for all days long!',
+    'You will overcome any difficulties you meet!',
+    'You will meet the uncertainty that can change your days!',
+    'You will get your efforts paid back pretty soon!'
+];
 
-button.addEventListener("click",function(){
+button.addEventListener("click", function () {
     let current_input = name.value;
     generate(current_input);
     restyle();
 });
 
 function generate(incUserInput) {
-    var rand_index = Math.floor(Math.random() * fortunes.length)
-    console.log(rand_index);
+    var rand_index = Math.floor(Math.random() * fortunes.length);
     var fortune_select = fortunes[rand_index];
     output.innerText = incUserInput + ", your fortune is: " + fortune_select;
 }
 
 function restyle() {
-    const colors = ['blue', 'orange', 'purple', 'green', 'brown'];
+    const colors = ['LightBlue', 'LemonChiffon', 'Magenta', 'Lime', 'SandyBrown'];
     const fonts = ['20px', '25px', '30px', '35px', '40px'];
-    const fontstyle = ['normal','italic'];
+    const fontstyle = ['normal', 'italic'];
 
     output.style.color = colors[Math.floor(Math.random() * colors.length)];
     output.style.fontSize = fonts[Math.floor(Math.random() * fonts.length)];
     output.style.fontStyle = fontstyle[Math.floor(Math.random() * fontstyle.length)];
 }
 
+var rellax = new Rellax('.rellax', {
+    center: false, 
+    wrapper: null, 
+    vertical: true,
+    horizontal: false 
+});
